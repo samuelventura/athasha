812f863d71a3b1f0e38ab7442285fa11db85e4b4
Samuel Ventura <samuel.ventura@yeico.com>
2022-04-08 03:15:26 -0500

    live sample

--- app/lib/athasha_web/router.ex
+++ app/lib/athasha_web/router.ex
@@ -1,23 +1,25 @@
 defmodule AthashaWeb.Router do
   use AthashaWeb, :router
+  import Phoenix.LiveView.Router
 
   pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_live_flash
-    plug :put_root_layout, {AthashaWeb.LayoutView, :root}
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
+    plug(:accepts, ["html"])
+    plug(:fetch_session)
+    plug(:fetch_live_flash)
+    plug(:put_root_layout, {AthashaWeb.LayoutView, :root})
+    plug(:protect_from_forgery)
+    plug(:put_secure_browser_headers)
   end
 
   pipeline :api do
-    plug :accepts, ["json"]
+    plug(:accepts, ["json"])
   end
 
   scope "/", AthashaWeb do
-    pipe_through :browser
+    pipe_through(:browser)
 
-    get "/", PageController, :index
+    get("/", PageController, :index)
+    live("/thermostat", ThermostatLive)
   end
 
   # Other scopes may use custom stacks.
@@ -36,9 +38,9 @@ defmodule AthashaWeb.Router do
     import Phoenix.LiveDashboard.Router
 
     scope "/" do
-      pipe_through :browser
+      pipe_through(:browser)
 
-      live_dashboard "/dashboard", metrics: AthashaWeb.Telemetry
+      live_dashboard("/dashboard", metrics: AthashaWeb.Telemetry)
     end
   end
 
@@ -48,9 +50,9 @@ defmodule AthashaWeb.Router do
   # node running the Phoenix server.
   if Mix.env() == :dev do
     scope "/dev" do
-      pipe_through :browser
+      pipe_through(:browser)
 
-      forward "/mailbox", Plug.Swoosh.MailboxPreview
+      forward("/mailbox", Plug.Swoosh.MailboxPreview)
     end
   end
 end
