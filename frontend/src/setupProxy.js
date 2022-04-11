d91e7954ed6a05d3aa39bfec4eea6d0c3f1f7565
Samuel Ventura <samuel.ventura@yeico.com>
2022-04-10 20:40:44 -0500

    setup proxy works

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -2,7 +2,6 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
-    console.log("app", app)
     app.use(
         createProxyMiddleware('/items/websocket', {
             target: 'http://localhost:4000',
