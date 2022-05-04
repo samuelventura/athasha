d13dd6545a5f3e95bfdd5c30116021caae949b6b
Samuel Ventura <samuel.ventura@yeico.com>
2022-05-04 12:47:40 -0500

    c# device id generator

---
+++ native/identity/Program.cs
@@ -0,0 +1,25 @@
+﻿using DeviceId;
+using SharpTools;
+
+Global.Catch((Exception ex) =>
+{
+    Console.Error.WriteLine("{0}", ex);
+    Environment.Exit(1);
+});
+
+string deviceId = new DeviceIdBuilder()
+    .AddMachineName()
+    .AddOsVersion()
+    .OnWindows(windows => windows
+        .AddProcessorId()
+        .AddMotherboardSerialNumber()
+        .AddSystemDriveSerialNumber())
+    .OnLinux(linux => linux
+        .AddMotherboardSerialNumber()
+        .AddSystemDriveSerialNumber())
+    .OnMac(mac => mac
+        .AddSystemDriveSerialNumber()
+        .AddPlatformSerialNumber())
+    .ToString();
+
+Console.WriteLine(deviceId);
