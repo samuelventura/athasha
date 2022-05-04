ccb9c46603cf635fb9da982b882251d17860367a
Samuel Ventura <samuel.ventura@yeico.com>
2022-05-04 13:44:45 -0500

    auth licenses monitor

--- backend/lib/athasha/auth.ex
+++ backend/lib/athasha/auth.ex
@@ -1,6 +1,7 @@
 defmodule Athasha.Auth do
   alias Athasha.License
   alias Athasha.Ports
+  alias Athasha.Raise
   alias Athasha.Repo
 
   def licenses() do
@@ -50,5 +51,34 @@ defmodule Athasha.Auth do
   def identity() do
     [line] = Ports.read_lines("identity")
     line
+  end
+
+  def start_link() do
+    spawn_link(&monitor_init/0)
+  end
+
+  defp monitor_check() do
+    IO.inspect({"monitor_check"})
+    # disable exceding licenses
+  end
+
+  # 1hr in millis
+  @check 1000 * 60 * 60
+
+  defp monitor_init() do
+    Process.send(self(), :check, @check)
+    monitor_loop()
+  end
+
+  defp monitor_loop() do
+    receive do
+      :check ->
+        monitor_check()
+        Process.send_after(self(), :check, @check)
+        monitor_loop()
+
+      other ->
+        Raise.error({:receive, other})
+    end
   end
 end
