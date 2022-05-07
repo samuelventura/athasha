6413d05f42b0552d5788d9a236e9cb999a0e736f
Samuel Ventura <samuel.ventura@yeico.com>
2022-05-06 21:20:16 -0500

    added native input to prompt for data and write to file because bat files are very limited

---
+++ native/input/Program.cs
@@ -0,0 +1,8 @@
+﻿var prompt = args[0];
+var trimmed = bool.Parse(args[1]);
+var file = args[2];
+Console.Write(prompt);
+var input = Console.ReadLine();
+if (input == null) return;
+if (trimmed) { input = input.Trim(); }
+File.WriteAllText(file, input);
