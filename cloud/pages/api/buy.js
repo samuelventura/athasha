439a6eeb0d0818d13f83e32f36fa164e22017d20
Samuel Ventura <samuel.ventura@yeico.com>
2022-05-18 00:34:59 -0500

    cloud tax rate failed tryout

--- cloud/pages/api/buy.js
+++ cloud/pages/api/buy.js
@@ -14,6 +14,7 @@ export default async function handler(req, res) {
       {
         price: process.env.PRICE_KEY,
         quantity: Number(req.body.qty),
+        //tax_rates: ['txr_1L0fV6J3rkKRlXZWgw1bJHfH', 'txr_1L0erYJ3rkKRlXZWB6s4LrsW'],
       },
     ],
     metadata: {
@@ -23,3 +24,7 @@ export default async function handler(req, res) {
   })
   res.status(200).json({ url: session.url })
 }
+
+//to test stripe region auto detection
+//ssh -N -D 9090 athasha.io
+//google-chrome --proxy-server="socks5://localhost:9090"
