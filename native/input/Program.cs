dd03665bf41b6363c2bf12e5ded00ce48f625ba1
Samuel Ventura <samuel.ventura@yeico.com>
2022-05-25 06:47:02 -0500

    native/input hidden password

--- native/input/Program.cs
+++ native/input/Program.cs
@@ -1,8 +1,31 @@
 ﻿var prompt = args[0];
 var trimmed = bool.Parse(args[1]);
-var file = args[2];
+var hidden = bool.Parse(args[2]);
+var file = args[3];
 Console.Write(prompt);
-var input = Console.ReadLine();
-if (input == null) return;
+var input = string.Empty;
+if (hidden) {
+    while (true)
+    {
+        var keyInfo = Console.ReadKey(intercept: true);
+        var key = keyInfo.Key;
+        if (key == ConsoleKey.Backspace && input.Length > 0)
+        {
+            Console.Write("\b \b");
+            input = input[0..^1];
+        }
+        else if (!char.IsControl(keyInfo.KeyChar))
+        {
+            Console.Write("*");
+            input += keyInfo.KeyChar;
+        }
+        if (key == ConsoleKey.Enter) break;
+    }
+}
+else 
+{
+    input = Console.ReadLine();
+    if (input == null) return;
+}
 if (trimmed) { input = input.Trim(); }
 File.WriteAllText(file, input);
--- setup/Password.bat
+++ setup/Password.bat
@@ -1,5 +1,5 @@
 @echo off
 cd %~dp0
 set file="%~dp0athasha.config.pwd"
-athasha\lib\ports-0.1.0\priv\dotnet\input.exe Password: true %file%
+athasha\lib\ports-0.1.0\priv\dotnet\input.exe Password: true true %file%
 call athasha\bin\athasha.bat rpc "Athasha.Auth.logout"
