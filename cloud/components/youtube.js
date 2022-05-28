5fccdebfff372d911f61d083973379636036201c
Samuel Ventura <samuel.ventura@yeico.com>
2022-05-28 01:50:15 -0500

    removed embedded youtube title

--- cloud/components/youtube.js
+++ cloud/components/youtube.js
@@ -10,7 +10,6 @@ const YoutubeEmbed = ({ embedId }) => (
             frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen
-            title="Embedded youtube"
         />
     </div>
 );
